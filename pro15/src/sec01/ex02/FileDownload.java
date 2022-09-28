610499d7f63fce1169141ad50eb1dcf95d012261
nsk5720 <nsg5720@naver.com>
2022-09-28 14:20:04 +0900

    게시판

---
+++ pro15/src/sec01/ex02/FileDownload.java
@@ -0,0 +1,34 @@
+package sec01.ex02;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/download.do")
+public class FileDownload extends HttpServlet{
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request, response);
+	}
+
+	protected void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html; charset=utf-8");
+		String file_refo = "C:\\file_repo";
+		String fileName = (String) request.getParameter("fileName");
+		System.out.println("fileName=" + fileName);
+		OutputStream out = response.getOutputStream();
+	}
+
+}
