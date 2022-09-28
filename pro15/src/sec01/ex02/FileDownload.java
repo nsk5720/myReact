b0f1f7b6e3f5d9f20b3d8839ae80ace25d329140
nsk5720 <nsg5720@naver.com>
2022-09-28 14:37:34 +0900

    fileDownload

--- pro15/src/sec01/ex02/FileDownload.java
+++ pro15/src/sec01/ex02/FileDownload.java
@@ -1,5 +1,7 @@
 package sec01.ex02;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 
@@ -25,10 +27,25 @@ public class FileDownload extends HttpServlet{
 	protected void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
 		response.setContentType("text/html; charset=utf-8");
-		String file_refo = "C:\\file_repo";
+		String file_repo = "C:\\file_repo";
 		String fileName = (String) request.getParameter("fileName");
 		System.out.println("fileName=" + fileName);
 		OutputStream out = response.getOutputStream();
+		
+		String downFile = file_repo + "\\" + fileName;
+		File f = new File(downFile);
+		response.setHeader("Cache-Control", "no-cache");
+		response.addHeader("Content-disposition", "attachment; fileName=" + fileName);
+		FileInputStream in = new FileInputStream(f);
+		byte[] buffer = new byte[1024 * 8];
+		while (true) {
+			int count = in.read(buffer);
+			if (count == -1)
+				break;
+			out.write(buffer, 0, count);
+		}
+		in.close();
+		out.close();
 	}
 
 }
