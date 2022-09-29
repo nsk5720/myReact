357d4d0a4e2945dc672bb24567d65c4fa9d8889c
nsk5720 <nsg5720@naver.com>
2022-09-29 14:35:35 +0900

    board

--- pro17/src/sec03/brd01/BoardController.java
+++ pro17/src/sec03/brd01/BoardController.java
@@ -2,6 +2,7 @@ package sec03.brd01;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -30,7 +31,31 @@ public class BoardController extends HttpServlet{
 
 
 	private void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		String nextPage = "";
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html; charset=utf-8");
+		String action = request.getPathInfo();	//요청할 내용을 가져온다.
+		System.out.println("action: " + action);
+		try {
+			List<ArticleVO> articlesList = new ArrayList<ArticleVO>();
+				if(action == null) {
+					articlesList = boardService.listArticles();
+					request.setAttribute("articlesList", articlesList);
+					nextPage = "/board01/listArticles.jsp";
+				} else if(action.equals("/listArticles.do")) {	/* action 값이 listArticles.do 이면 전체글 조회*/
+					articlesList = boardService.listARticles();
+					/*조회된 글 목록을 articleList로 바인딩 한 후 listArticles.jsp로 포워딩*/
+					request.setAttribute("articlesList", articlesList);
+					nextPage = "/board01/listArticles.jsp";
+				} else {
+					nextPage = "/board01/listArticles.jsp";
+				}
+				
+				RequestDispatcher dispatch = request.getRequestDispatcher(nextPage);
+				dispatch.forward(request, response);
+		}	catch (Exception e){
+			e.printStackTrace();
+		}
 	}
 	
 	
